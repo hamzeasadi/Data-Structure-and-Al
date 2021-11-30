9142681595f482c04b7c43ef25fddadb378a47ae
hamzeasadi <hamzeasadi1@gmail.com>
2021-11-30 23:07:24 +0000

    implement the function

--- basic/introduction.py
+++ basic/introduction.py
@@ -6,10 +6,19 @@ import cmath
 # Python Program to Find the Square Root
 def Root(x):
     """Find the root of the given value"""
+    root_vale = cmath.sqrt(x)
+    if isinstance(root_vale, complex):
+        print(f"the root value of {x} is a complex number")
+    else:
+        print(f"the root value is a real number")
+
+    return root_vale
 
 
 
 
 
 if __name__ == '__main__':
-    pass
+    num = complex(input("please enter a number to calculate the root value: "))
+    root_num = Root(num)
+    print(root_num)
