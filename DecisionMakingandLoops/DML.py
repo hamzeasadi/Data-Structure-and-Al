3ce1455140023924b6c2a8ea60f46de14c0efb0c
hamzeasadi <hamzeasadi1@gmail.com>
2021-12-01 15:13:34 +0000

    check range function for being iterable and iterator

--- DecisionMakingandLoops/DML.py
+++ DecisionMakingandLoops/DML.py
@@ -4,3 +4,33 @@ import sys
 import os
 import cms
 from fractions import Fraction
+
+
+def range_test(range_vars):
+    start, stop, step = range_vars.split(',')
+    start, stop, step = eval(start), eval(stop), eval(step)
+    range_obj = range(start=start, stop=stop, step=step)
+    range_list = list(range_obj)
+    print(f"the range object is: {range_obj}")
+    print(f"list(range_obj): {range_list}")
+    # print(f"dir(range_obj): {dir(range_obj)}")
+    range_obj_attr = dir(range_obj)
+    if '__next__' in range_obj_attr:
+        print(f"range_obj is an iterator")
+    elif '__iter__' in range_obj_attr:
+        print(f"range_obj is an iterable")
+    else:
+        print("range_obj is not an iterable and iterator")
+
+
+
+
+
+
+
+
+
+
+
+if __name__ == '__name__':
+    pass
