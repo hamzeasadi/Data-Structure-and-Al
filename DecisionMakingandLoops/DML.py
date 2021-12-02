53b290b96d4e86f055af44dbd0ea6294a474d5d3
hamzeasadi <hamzeasadi1@gmail.com>
2021-12-02 21:30:31 +0000

    implement factorial function

--- DecisionMakingandLoops/DML.py
+++ DecisionMakingandLoops/DML.py
@@ -149,7 +149,13 @@ def factorial(n: int):
     Raises:
         It will raise an value error if n>100
     """
-
+    if n > -1 and n < 101:
+        if n == 0 :
+            return 1
+        else:
+            return factorial(n-1)*n
+    else:
+        raise ValueError
 
 
 if __name__ == '__main__':
