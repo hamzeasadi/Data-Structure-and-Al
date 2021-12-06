0e7d32fd85854ae04e25a5764855918cef11ed62
hamzeasadi <hamzeasadi1@gmail.com>
2021-12-06 07:51:06 +0000

    listMult docstring

--- DataType/list.py
+++ DataType/list.py
@@ -10,7 +10,14 @@ random.seed(42)
 
 # def listMult function
 def listMult(arr: list):
-    pass
+    """listMult function accept a list and multiply its elements together
+    args:
+        arr: an arbitrary size list with integers, float or coplex values
+    return:
+        multValue: return the multiplication of the list elements
+    Raises:
+        ValueError: if list elements are not a number
+    """
 
 
 
