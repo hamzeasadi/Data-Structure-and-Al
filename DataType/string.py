66f09a6007aa617d1fc555d84a2ad6018fcbb433
hamzeasadi <hamzeasadi1@gmail.com>
2021-12-17 14:26:42 +0000

    count function

--- DataType/string.py
+++ DataType/string.py
@@ -2,10 +2,19 @@ import random
 import os
 import cmath
 
+def count(string: str, character):
+    cnt = 0
+    for x in string:
+        if x==character:
+            cnt+=1
+    return cnt
+
 
 
 def main():
-    pass
+    mystr = 'pycharm'
+    del mystr
+    print(mystr)
 
 
 if __name__ == '__main__':
