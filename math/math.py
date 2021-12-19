02a98f2c133cd7412be1ddbc179bcacf28e84749
hamzeasadi <hamzeasadi1@gmail.com>
2021-12-19 11:06:47 +0000

    handshake function with its docstring

--- math/math.py
+++ math/math.py
@@ -9,8 +9,20 @@ def findPoint(px, py, qx, qy):
     r = [2*qx-px, 2*qy-py]
     return r
 
+# define maximumDraws function
+def maximumDraws():
+    pass
 
-
+# define handshake
+def handshake(n):
+    """if we have n people how many hand shakes it is gonna be if
+    they handshake each other just ones
+    args:
+        n: number of people
+    returns:
+        hs: number of handshakes
+    """
+    pass
 
 
 def main():
