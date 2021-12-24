f289e1afd347b7cee8930ef9f376cff0013cb856
hamzeasadi <hamzeasadi1@gmail.com>
2021-12-24 11:02:48 +0000

    implement count function of itertool

--- basic/itertool.py
+++ basic/itertool.py
@@ -2,15 +2,22 @@ import itertools
 from itertools import count
 
 
+def mycount(start, step):
+	i=0
+	while True:
+		yield start + i*step	
+		i+=1
 
-
+def palindrome(n):
+	out = ((10**n)//9)**2
+	return out
 
 def main():
-	
-	for i in count(12, 3):
-		print(i)
-		if i>30:
-			break
+	for i in mycount(1, 1):
+		print(palindrome(i))
+		if i>8:
+			break	
+
 
 if __name__ == '__main__':
 	main()
