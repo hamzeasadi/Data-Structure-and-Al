23b8f9a2675256f2386083f7a99083c08c1e3ce8
hamzeasadi <hamzeasadi1@gmail.com>
2021-12-25 19:22:56 +0000

    shoe counter challenge

--- DataType/dictonary.py
+++ DataType/dictonary.py
@@ -10,11 +10,10 @@ from faker import Faker
 
 
 def main():
-    fake = Faker()
-    dictionary = open('list.py', mode='r', encoding='utf-8')
-    print(dir(dictionary))
-    for line in dictionary.readlines():
-        print(line)
+    from collections import Counter
+    X = int(input().strip())
+    shoe_size_list = map(int, input().strip().split())
+    size_counter = Counter(shoe_size_list)
 
 if __name__ == '__main__':
     main()
--- basic/itertool.py
+++ basic/itertool.py
@@ -24,12 +24,23 @@ def orderper(s, k):
 	return out
 		
 
+
 def main():
 
-	x = list(input().strip())
-	print(x)
+	N = int(input("enter the lenght of the whole string: ")).strip()	
+	x = input(f"enter the string with lenght {N}: ").strip().split()
+	k = int(input("enter the subset lenght: ").strip())
+	
 	x.sort()
-	print(x)
+	ferq = lambda x: x[0]
+	C = itertools.groupby(x, freq)
+	for c in C:
+		if key=='a':
+			prob = 0.
+			for i in range(1, k)
+				math.comb(k, i)*(len(list(group))
+
+
 
 
 
