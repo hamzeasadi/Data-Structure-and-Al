332b05f4d80ba80fef89453b7d984b9b4820a326
hamzeasadi <hamzeasadi1@gmail.com>
2021-12-27 23:09:34 +0000

    set screen for the game

--- basic/advance.py
+++ basic/advance.py
@@ -9,13 +9,52 @@ def multiplierof(n):
         print(f"{n}x{x} = {n*x}")
     return multiplier
 
+def my_decorator(func):
+    def wrapper():
+        print("sth happen before the function is called!!")
+        func()
+        print("sth happen after the function is called!!")
 
+    return wrapper
+
+
+@my_decorator
+def say_whee():
+    print("whee!!")
+
+def do_twice(func):
+    def wrapper(*args, **kwargs):
+        func(*args, **kwargs)
+        func(*args, **kwargs)
+    return wrapper
+
+@do_twice
+def greet(name):
+    print(f"Hello {name}")
+
+class Celsius():
+    def __init__(self, temp):
+        self.__temp = temp
+
+    def to_farenhit(self):
+        return self.__temp*1.8 + 32
+
+
+
+
+"""
+a decorator function real story is as following
+@decorator
+def myfunction(): === myfunction = decorator(myfunction)
+    ....
+"""
 
 def main():
-    n, x = map(eval, input("Please enter n, x: ").strip(' ,').split(','))
-    mymul = multiplierof(n)
-    mymul(x)
-    print(dir(multiplierof))
+
+    # say_whee = my_decorator(say_whee)
+    # say_w()
+    # print(say_w)
+    greet(name='asqar')
 
 
 
--- pygame/intro_pygame.py
+++ pygame/intro_pygame.py
@@ -1,31 +1,21 @@
 import random
-import pygame
+import pygame as pg
 
-SCREEN_WIDTH = 800
-SCREEN_HIGHT = 600
 
-black = (0,0,0)
-white = (255,255,255)
+
+# hyper parameters setting
+SW = 800
+SH = 600
+
+
 
 
 def main():
     pygame.init()
-    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HIGHT))
-    pygame.display.set_caption('SIMPLE GAME')
-    clock = pygame.time.Clock()
-
-    running = True
-    while running:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                running = False
-            print(event)
+    screen = pygame.display.set_mode([SW, SH])
 
 
-        pygame.display.update()
-        clock.tick(60)
 
-    pygame.quit()
 
 
 if __name__ == '__main__':
