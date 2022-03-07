4d080f765c282b0a5387c17f83b827b138f96bed
hamzeasadi <hamzeasadi1@gmail.com>
2022-03-07 19:31:35 +0000

    cone volume function

--- chp1.py
+++ chp1.py
@@ -1,12 +1,21 @@
 import os
+import math
+
 
 def wordCount(sentence):
     x = sentence.strip().count(' ') + 1
     print(x)
 
+def coneVolume(r, h):
+    volume = 3.14*r**2*(h/3)
+    print(volume)
 
 def main():
-    wordCount(input("enter your sentence: "))
+    r = input().strip()
+    h = input().strip()
+    r, h = eval(r), eval(h)
+    coneVolume(r, h)
+
 
 if __name__ == '__main__':
     main()
