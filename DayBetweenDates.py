4a594722ade535e522a1fbde1a670f17c9acb37e
hamzeasadi <hamzeasadi1@gmail.com>
2022-04-25 12:47:37 +0100

    transfer from str to int

--- DayBetweenDates.py
+++ DayBetweenDates.py
@@ -8,10 +8,45 @@ my_parser = argparse.ArgumentParser(description="this parser takes two argument 
 my_parser.add_argument('--birth_date', '-bd', action='store', nargs=1, type=str, required=True)
 my_parser.add_argument('--end_date', '-ed', action='store', nargs=1, type=str, default=str(date.today()))
 
+daysInMonths = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+
+def nextDay(*date):
+    year, month, day = date[0], date[1], date[2]
+    if day < 30:
+        return year, month, day+1
+    elif month < 12 :
+        return year, month+1, 1
+    else:
+        return year+1, 1, 1
+
+def dayBetweenDates(*dates):
+    year1, month1, day1 = dates[0], dates[1], dates[2]
+    year2, month2, day2 = dates[3], dates[4], dates[5]
+    date1 = dates[0:3]
+    date2 = dates[3:]
+    days = 0
+    while isBefore(date1, date2):
+        days+=1
+        date1 = nextDay(date1)
+
+    return days
+
+    
+
+def isBefore(date1, date2):
+    if date1[0]<date2[0]:
+        return True
+    elif (date1[0]==date2[0]) and (date1[1]<date2[1]):
+        return True
+    elif (date1[1]==date2[1]) and (date1[2]<= date2[2]):
+        return True
+    else:
+        return False
+
 
 def main():
-    birthdate = my_parser.parse_args().birth_date
-    enddate = my_parser.parse_args().end_date
+    birthdate = list(map(int, my_parser.parse_args().birth_date[0].split('-')))
+    enddate = list(map(int, my_parser.parse_args().end_date.split('-')))
     print(enddate)
     print(birthdate)
 
